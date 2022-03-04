670adc8d11e75f419007f4124620a219f9f63d59
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-03-03 21:19:59 -0500

    Creating mercadopago integration in backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,13 @@ import express from "express"
 import mongoose from "mongoose"
 import Product from "./models/product.js"
 import Order from "./models/order.js"
+import mercadopago from "mercadopago"
 
 const router = express.Router()
+
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN,
+})
 
 router.get("/products", async (req, res, next) => {
   try {
@@ -26,12 +31,41 @@ router.post("/products", async (req, res, next) => {
 
 router.post("/orders", async (req, res) => {
   const { products } = req.body
+  const items = []
   for (let i = 0; i < products.length; i++) {
     const product = await Product.findById(new mongoose.Types.ObjectId(products[i])).lean()
     products[i] = product
+    if (product) {
+      items.push({
+        title: product.name,
+        quantity: 1,
+        currency_id: "PEN",
+        unit_price: product.price,
+        picture_url: product.image,
+      })
+    }
   }
   const order = await Order.create({ products })
-  res.json(order)
+  mercadopago.preferences
+    .create({
+      items,
+      notification_url: "https://hookbin.com/b90rzJ70Q6cKGq00yE79",
+      back_urls: {
+        success: "http://localhost:3000/",
+        pending: "http://localhost:3000/",
+        failure: "http://localhost:3000/",
+      },
+    })
+    .then(function (response) {
+      res.json({
+        id: response.body.id,
+        sandbox_init_point: response.body.sandbox_init_point,
+        order,
+      })
+    })
+    .catch(function (error) {
+      res.json({ items })
+    })
 })
 
 export default router
