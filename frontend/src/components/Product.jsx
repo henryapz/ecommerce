0b997d04c1c4534ea23241315eede5dfa5db4fe3
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-03-03 21:36:44 -0500

    Adding some styles to card

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -9,11 +9,17 @@ function Product({ product }) {
   function addToCart(product) {
     dispatch({ type: "ADD_TO_CART", payload: product })
   }
-
+  const style = {
+    maxHeight: "140px",
+    maxWidth: "140px",
+    width: "auto",
+    height: "auto",
+    alignSelf: "center",
+  }
   return (
-    <Col xs={6}>
-      <Card>
-        <Card.Img variant="top" src={product.image} />
+    <Col xs={6} className="mb-3">
+      <Card style={{ height: "100%" }}>
+        <Card.Img variant="top" src={product.image} style={style} />
         <Card.Body>
           <Card.Title>{product.name}</Card.Title>
           <Card.Text>{product.description}</Card.Text>
